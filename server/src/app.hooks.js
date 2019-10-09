d398456e29c99cfb2caab058692affaee07127d6
smartzhu@gmail.com <smartzhu@gmail.com>
2019-10-09 14:12:46 -0400

    improved transcation

--- server/src/app.hooks.js
+++ server/src/app.hooks.js
@@ -1,14 +1,39 @@
 // Application hooks that run for every service
+const TransactionManager = require('feathers-mongoose').TransactionManager
+const isTransactionEnable = true
+const skipPath = ['authentication']
+//这是跳过哪些模块不做事务处理,
+//比如 删除board的时候模块是boards,写在这里board就被直接删除了
 
 module.exports = {
   before: {
-    all: [],
+    all: [
+      // async context => {
+      //   console.log("context path")
+      //   console.log(context.path)
+      //   // //console.log(context)
+      //   // const session = context.params.mongoose.session
+      //   // console.log(session)
+      //   // const lists = mongoose.model('lists')
+      //   // await lists.remove({ boardId: context.id }).session(session)
+      //   // //await lists.remove({ boardId: context.id },{session:session})
+      //   return context
+      // }
+    ],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [
+      async context => {
+        if (isTransactionEnable) {
+          TransactionManager.beginTransaction(context, skipPath)
+        }
+        //console.log("hook path")
+        //console.log(context.path)
+      }
+    ]
   },
 
   after: {
@@ -18,7 +43,14 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [
+      ()=>{
+        throw "hello"
+      },
+      TransactionManager.commitTransaction,
+      
+      //TransactionManager.rollbackTransaction
+    ]
   },
 
   error: {
@@ -28,6 +60,8 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [
+      TransactionManager.rollbackTransaction
+    ]
   }
 };
--- server/src/hooks/lists.js
+++ server/src/hooks/lists.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
+const { authenticate } = require('@feathersjs/authentication').hooks
 
 
-async function remvoeList(context, boardId){
-    console.log("==121212==here remove list auto")
-    const lists = mongoose.model('lists')
-    await lists.remove({boardId:boardId})
-    return context
+// async function remvoeList(context, boardId){
+//     console.log("==121212==here remove list auto")
+//     const lists = mongoose.model('lists')
+//     await lists.remove({boardId:boardId})
+//     return context
+// }
+
+const removeList = async context => {
+  console.log("inboard context path")
+  console.log(context.path)
+  //console.log(context)
+  const session = context.params.mongoose.session
+  console.log(session)
+  const lists = mongoose.model('lists')
+  await lists.remove({ boardId: context.id }).session(session)
+  return context
 }
 
 module.exports = {
-  remvoeList
+  removeList
 };
